cd466e1384181c33404e37520d5e7ab5252eaf3d
Btojaka <bruthtoj@gmail.com>
2023-09-11 14:13:01 +0200

    change url

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:Btojaka/vue-chocolate.git main:gh-pages
+ git push -f git@github.com:Btojaka/todoVue-ghpages.git main:gh-pages
 
 cd -
